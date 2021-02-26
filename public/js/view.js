2155d71bf6df6caf35a4d383fc17e48ad3de3754
EgiVilza <egivilza@gmail.com>
2021-02-25 21:29:39 -0600

    added some html content

--- public/js/view.js
+++ public/js/view.js
@@ -2,59 +2,67 @@
 //Add to amount left
 
 //If lost
-//Check if amount left is 0, if so, end game and say try again?
+//Check if amount left is 0, if so, end game and add an option to try again
 
 //Things to display
 //House's cards
 //Player's cards
 
+//Default Bet Amount
 var defaultBetAmount = 0;
+
+//Default Amount Left
 var defaultAmountLeft = 5000;
-$("#amountLeft").val(defaultAmountLeft);
+//$("#amountLeft").val(defaultAmountLeft);
 
-//Place bet
+//Place bet, Just needs "start game" function
 const bet = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const bet = parseInt($("#bet").val());
+  //Bet Amount
+  const bet = parseInt($("#bet").val());
 
-    if (bet > defaultAmountLeft){
-        alert("Bet is higher than the amount left");
-        return;
-    }
+  //If bet amount is greater than amount left, alert user
+  if (bet > defaultAmountLeft){
+    alert("Bet is higher than the amount left");
+    return;
+  }
 
-    defaultAmountLeft = defaultAmountLeft - bet;
-    $("#amountLeft").val(defaultAmountLeft)
+  //Updates amount left after bet has been placed
+  defaultAmountLeft = defaultAmountLeft - bet;
+  $("#amountLeft").val(defaultAmountLeft)
 
-    defaultBetAmount = defaultBetAmount + bet;
-    $("#betAmount").val(defaultBetAmount);
+  //Updates bet amount
+  defaultBetAmount = defaultBetAmount + bet;
+  $("#betAmount").val(defaultBetAmount);
 
-    //Start Game
-}
+  //Start Game
+};
 
 //Hit: Take another card and place it on the table
 const hit = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    //Hit another card
-}
+  //Hit another card
+};
 
 //Stay: Play with the cards you have
 const stay = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    //Let the dealer finish
-}
+  //Let the dealer finish
+};
 
 //Finish: End the game and post the Amount Left to the scoreboard
 const finish = (e) => {
-    e.preventDefault();
-    const amountLeft = $("#amountLeft").val();
-    const name = prompt('Please enter your name');
-    const score = {Name: name,Score: amountLeft};
+  e.preventDefault();
+  const amountLeft = $("#amountLeft").val();
+  const name = prompt("Please enter your name");
+  const score = { Name: name, Score: amountLeft };
 
-    return score
-}
+  //Add score to database
+  console.log(score);
+};
 
 //Button "click" Event Listeners
 $("#placeBet").on("click", bet);
