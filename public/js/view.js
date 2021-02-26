c72b408a298b239e2ded430adc99974fde00b4f1
EgiVilza <egivilza@gmail.com>
2021-02-25 21:01:39 -0600

    Add some button functionality

--- public/js/view.js
+++ public/js/view.js
@@ -1,38 +1,59 @@
-//click on the buttons to perform the actions
-//Place Bet - Add to the bet amount, subtract amount left
-//Hit - Add another card
-//Stay - End Hitting
-//Finish - Prompt for username and post to scoreboard
-
 //If won
 //Add to amount left
 
 //If lost
-//Subtract to amount left
 //Check if amount left is 0, if so, end game and say try again?
 
 //Things to display
 //House's cards
 //Player's cards
 
-var betAmount = 0
+var defaultBetAmount = 0;
+var defaultAmountLeft = 5000;
+$("#amountLeft").val(defaultAmountLeft);
 
+//Place bet
 const bet = (e) => {
     e.preventDefault();
+
     const bet = parseInt($("#bet").val());
-    betAmount = betAmount + bet;
-    $("#betAmount").val(betAmount);
+
+    if (bet > defaultAmountLeft){
+        alert("Bet is higher than the amount left");
+        return;
+    }
+
+    defaultAmountLeft = defaultAmountLeft - bet;
+    $("#amountLeft").val(defaultAmountLeft)
+
+    defaultBetAmount = defaultBetAmount + bet;
+    $("#betAmount").val(defaultBetAmount);
+
+    //Start Game
 }
 
+//Hit: Take another card and place it on the table
 const hit = (e) => {
     e.preventDefault();
+
+    //Hit another card
 }
 
+//Stay: Play with the cards you have
 const stay = (e) => {
     e.preventDefault();
+
+    //Let the dealer finish
 }
+
+//Finish: End the game and post the Amount Left to the scoreboard
 const finish = (e) => {
     e.preventDefault();
+    const amountLeft = $("#amountLeft").val();
+    const name = prompt('Please enter your name');
+    const score = {Name: name,Score: amountLeft};
+
+    return score
 }
 
 //Button "click" Event Listeners
