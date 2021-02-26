29a29759115f14374dce7524bbe1d95d2a289def
EgiVilza <egivilza@gmail.com>
2021-02-26 17:49:37 -0600

    Added handlebars code

---
+++ controllers/casinoController.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+// // Import the model (cat.js) to use its database functions.
+// const cat = require('../models/');
+
+// router.get('/', (req, res) => {
+
+// })
+
+router.get('/', (req, res) => {
+  res.render("index");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,39 +1,29 @@
-// // Dependencies
-// const express = require('express');
-// const exphbs = require('express-handlebars');
+// Dependencies
+const express = require('express');
+const exphbs = require('express-handlebars');
 
-// // Create an instance of the express app.
-// const app = express();
+// Create an instance of the express app.
+const app = express();
+app.use('/public', express.static('public'))
 
-// // Set the port of our application
-// // process.env.PORT lets the port be set by Heroku
-// const PORT = process.env.PORT || 8080;
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
 
-// // Set Handlebars as the default templating engine.
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// app.set('view engine', 'handlebars');
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// // Start our server so that it can begin listening to client requests.
-// // Log (server-side) when our server has started
-// app.listen(PORT, () =>
-//   console.log(`Server listening on: http://localhost:${PORT}`)
-// );
+// Set Handlebars as the default templating engine.
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-// // Data
-// const lunches = [
-//     {
-//       lunch: 'Beet & Goat Cheese Salad with minestrone soup.',
-//     },
-//     {
-//       lunch: 'Pizza, two double veggie burgers, fries with a Big Gulp',
-//     },
-//   ];
-  
-//   // Routes
-//   app.get('/weekday', (req, res) => {
-//     res.render('index', lunches[0]);
-//   });
-  
-//   app.get('/weekend', (req, res) => {
-//     res.render('index', lunches[1]);
-//   });
+const routes = require('./controllers/casinoController.js')
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+// Log (server-side) when our server has started
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
