85cfd82c31b3f4115b4d5e03910e1a7bd3331e7e
EgiVilza <egivilza@gmail.com>
2021-02-28 15:54:16 -0600

    Added cards images and updated game functionality

--- public/js/view.js
+++ public/js/view.js
@@ -1,37 +1,39 @@
+//const  = prompt("What is your name");
+
+//Classes
+const ddeck = new deck(); //generatedDeck from the api
+const dplayer = new player("Egi", 5000); //generatedPlayer from the api
+const ddealer = new dealer(); //generatedDealer from the api
+
 //Default Bet Amount
 var defaultBetAmount = 0;
 // dplayer.currentBet, if we want
-​
 //Default Amount Left
 var defaultAmountLeft = dplayer.bank;
-​
-//
-let ddeck = {};//generatedDeck from the api
-let dplayer = {};//generatedPlayer from the api
-let ddealer = {};//generatedDealer from the api
-​
+
 //Place bet, Just needs "start game" function
-const bet = (e) => {
+const bet = e => {
   e.preventDefault();
-​
-  //Bet Amount
-  const bet = parseInt($("#bet").val());
-  dplayer.currentBet = bet
-​
+
   //If bet amount is greater than amount left, alert user
   if (bet > defaultAmountLeft) {
     alert("Bet is higher than the amount left");
     return;
   }
-​
+
+  //Bet Amount
+  const bet = parseInt($("#bet").val());
+  dplayer.currentBet = bet;
+  console.log(dplayer.currentBet);
+
   //Updates amount left after bet has been placed
   defaultAmountLeft = defaultAmountLeft - bet;
   $("#amountLeft").val(defaultAmountLeft);
-​
+
   //Updates bet amount
   defaultBetAmount = defaultBetAmount + bet;
   $("#betAmount").val(defaultBetAmount);
-​
+
   //Start Game
   //Player draws two cards to hand
   dplayer.drawCard(ddeck);
@@ -39,99 +41,93 @@ const bet = (e) => {
   //Dealer completes first turn
   ddealer.dealerTurn(ddeck);
 };
-​
+
 //Hit: Take another card and place it on the table
-const hit = (e) => {
+const hit = e => {
   e.preventDefault();
   dplayer.drawCard(ddeck);
-  if(dplayer.bust){
+  if (dplayer.bust) {
     //Player's hand busted, player loses bet
   }
-  if(dplayer.score === 21){
+  if (dplayer.score === 21) {
     //Player wins, gets 1.5 times bet
   }
   //Hit another card
 };
-​
+
 //Stay: Play with the cards you have
-const stay = (e) => {
+const stay = e => {
   e.preventDefault();
   dplayer.setStand();
   //Completes every dealer turn until dealer busts or decides to stand
-  for(let z = 0; !ddealer.bust && !ddealer.stand; z++){
-    ddealer.dealerTurn(ddeck)
+  for (let z = 0; !ddealer.bust && !ddealer.stand; z++) {
+    ddealer.dealerTurn(ddeck);
   }
-  if(ddealer.bust){
+  if (ddealer.bust) {
     //Player wins 2x bet
-  }
-  else if(dplayer.score>ddealer.score){
+  } else if (dplayer.score > ddealer.score) {
     //Player wins 2x bet
-  }
-  else if(dplayer.score === ddealer.score){
+  } else if (dplayer.score === ddealer.score) {
     //Nobody wins, player gets bet back
-  }
-  else{
+  } else {
     //Player loses because dealer has a higher score, player loses the bet
   }
-
 };
-​
+
 //Finish: End the game and post the Amount Left to the scoreboard
-const finish = (e) => {
+const finish = e => {
   e.preventDefault();
   const amountLeft = $("#amountLeft").val();
-  const name = prompt("Please enter your name");
+  const name = dplayer.name;
   const score = { Name: name, Score: amountLeft };
-​
+
   //Add score to database
   return score;
 };
-​
-const insertPlayerAmount = (e) => {
-  e.preventDefault()
-  const playerAmount = $("#amountLeft").val()
-  const dplayer = {
-      
-  }
-  if (dplayer.bank)
-      fetch('/startUp', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(amountTest)
-      })
-      .then((results) => results.json())
-      .then(getPlayer())
-};
-​
-​
-const getPlayer = () => {
-  fetch('/startUp', {
-  method: 'GET',
-  headers: {
-      'Content-Type': 'application/json',
-  },
-  })
-  .then((response) => response.json())
-  .then((data) => {
-      defaultAmountLeft = data
-      console.log(de)
-  });
-};
-​
+
 //Button "click" Event Listeners
 $("#placeBet").on("click", bet);
 $(".hitButton").on("click", hit);
 $(".stayButton").on("click", stay);
 $(".finishButton").on("click", finish);
-​
+
 //If won
 //Add to amount left
-​
+
 //If lost
 //Check if amount left is 0, if so, end game and add an option to try again
-​
+
 //Things to display
 //House's cards
 //Player's cards
+
+// const insertPlayerAmount = e => {
+//   e.preventDefault();
+//   const playerAmount = $("#amountLeft").val();
+//   const dplayer = {};
+//   if (dplayer.bank) {
+//     fetch("/startUp", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify(amountTest)
+//     })
+//       .then(results => results.json())
+//       .then(getPlayer());
+//   }
+// };
+
+// const getPlayer = () => {
+//   fetch("/startUp", {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       defaultAmountLeft = data;
+//       console.log(de);
+//     });
+// };
--- server.js
+++ server.js
@@ -1,32 +1,28 @@
 // Dependencies
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const exphbs = require("express-handlebars");
 
 // Create an instance of the express app.
 const app = express();
-app.use('/public', express.static('public'))
+app.use("/public", express.static("public"));
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 const PORT = process.env.PORT || 8080;
 
-
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 // Set Handlebars as the default templating engine.
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-const routes = require('./controllers/casinoController.js')
+const routes = require("./controllers/casinoController.js");
 
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 // Log (server-side) when our server has started
-app.listen(PORT, () =>
-  console.log(`Server listening on: http://localhost:${PORT}`)
+app.listen(PORT, () => console.log(`Server listening on: http://localhost:${PORT}`)
 );
-
