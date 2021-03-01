c6a6b23503607873955bc4c6db7312b532dc65d6
EgiVilza <egivilza@gmail.com>
2021-02-28 19:07:59 -0600

    Added game functionality

--- public/js/view.js
+++ public/js/view.js
@@ -1,58 +1,67 @@
-//const  = prompt("What is your name");
+//let name = prompt("Please enter your name");
 
 //Classes
 const ddeck = new deck(); //generatedDeck from the api
 const dplayer = new player("Egi", 5000); //generatedPlayer from the api
 const ddealer = new dealer(); //generatedDealer from the api
-
-//Default Bet Amount
-var defaultBetAmount = 0;
-// dplayer.currentBet, if we want
-//Default Amount Left
-var defaultAmountLeft = dplayer.bank;
+ddeck.shuffle();
 
 //Place bet, Just needs "start game" function
 const bet = e => {
   e.preventDefault();
 
+  //Bet Amount from the front end
+  const betAmount = parseInt($("#bet").val());
+
   //If bet amount is greater than amount left, alert user
-  if (bet > defaultAmountLeft) {
+  if (betAmount > dplayer.bank) {
     alert("Bet is higher than the amount left");
+    return;
+  } else if ($("#bet").val() === "" || $("#bet").val() === 0) {
+    alert("Must add bet");
     return;
   }
 
-  //Bet Amount
-  const bet = parseInt($("#bet").val());
-  dplayer.currentBet = bet;
-  console.log(dplayer.currentBet);
+  //Update Current Bet
+  dplayer.currentBet = betAmount;
 
   //Updates amount left after bet has been placed
-  defaultAmountLeft = defaultAmountLeft - bet;
-  $("#amountLeft").val(defaultAmountLeft);
+  dplayer.bank = dplayer.bank - betAmount;
+  $("#amountLeft").val(dplayer.bank);
 
-  //Updates bet amount
-  defaultBetAmount = defaultBetAmount + bet;
-  $("#betAmount").val(defaultBetAmount);
+  //Updates bet amount on the front end
+  $("#betAmount").val(dplayer.currentBet);
 
   //Start Game
   //Player draws two cards to hand
   dplayer.drawCard(ddeck);
   dplayer.drawCard(ddeck);
+
+  //Render Player's Cards
+  renderPlayersCards();
+
   //Dealer completes first turn
   ddealer.dealerTurn(ddeck);
+
+  //Render Dealer's Cards
+  renderDealersCards();
+
+  //Score results are checked if bust or blackjack
+  checkResults();
 };
 
 //Hit: Take another card and place it on the table
 const hit = e => {
   e.preventDefault();
+
+  //Player draws card
   dplayer.drawCard(ddeck);
-  if (dplayer.bust) {
-    //Player's hand busted, player loses bet
-  }
-  if (dplayer.score === 21) {
-    //Player wins, gets 1.5 times bet
-  }
-  //Hit another card
+
+  //Card is rendered
+  renderPlayersCards();
+
+  //Score results is checked if bust or blackjack
+  checkResults();
 };
 
 //Stay: Play with the cards you have
@@ -62,15 +71,24 @@ const stay = e => {
   //Completes every dealer turn until dealer busts or decides to stand
   for (let z = 0; !ddealer.bust && !ddealer.stand; z++) {
     ddealer.dealerTurn(ddeck);
+    renderDealersCards();
   }
+
   if (ddealer.bust) {
     //Player wins 2x bet
+    dplayer.bank = dplayer.bank + dplayer.currentBet * 2;
+    gameText("Dealer Bust, gain 2x the bet");
   } else if (dplayer.score > ddealer.score) {
     //Player wins 2x bet
+    dplayer.bank = dplayer.bank + dplayer.currentBet * 2;
+    gameText("Player wins, gain 2x the bet");
   } else if (dplayer.score === ddealer.score) {
     //Nobody wins, player gets bet back
+    dplayer.bank = dplayer.bank + dplayer.currentBet;
+    gameText("Tie");
   } else {
     //Player loses because dealer has a higher score, player loses the bet
+    gameText("Dealer Scored Higher, lost bet");
   }
 };
 
@@ -81,8 +99,83 @@ const finish = e => {
   const name = dplayer.name;
   const score = { Name: name, Score: amountLeft };
 
-  //Add score to database
+  //Return score to be added onto the leaderboard and the database
   return score;
+};
+
+//Render Player's Cards
+const renderPlayersCards = () => {
+  const playersHand = $(".playersHand");
+  playersHand.empty();
+
+  dplayer.hand.forEach(element => {
+    const cardImage = $("<img>");
+    const imgSrc = "/Public/Images/CardFaces/" + element.image;
+
+    cardImage.attr({
+      src: imgSrc,
+      class: "cards"
+    });
+
+    playersHand.append(cardImage);
+  });
+};
+
+//Render Dealer's Cards
+const renderDealersCards = () => {
+  const dealersHand = $(".dealersHand");
+  dealersHand.empty();
+
+  ddealer.hand.forEach(element => {
+    const cardImage = $("<img>");
+    const imgSrc = "/Public/Images/CardFaces/" + element.image;
+
+    cardImage.attr({
+      src: imgSrc,
+      class: "cards"
+    });
+
+    dealersHand.append(cardImage);
+  });
+};
+
+//Check if player/dealer busted/won/tie
+const checkResults = () => {
+  //Check if player bust or hits BlackJack
+  if (dplayer.bust) {
+    //Player's hand busted, player loses bet, round is over
+    gameText("Bust");
+  } else if (dplayer.score === 21) {
+    const hand = dplayer.hand;
+    let betMultiplier = 1.5;
+
+    if (hand.length === 2) {
+      betMultiplier = 2;
+    } else {
+      betMultiplier = 1.5;
+    }
+
+    dplayer.bank = dplayer.bank + dplayer.currentBet * betMultiplier;
+    gameText("BlackJack");
+  }
+};
+
+const nextRound = () => {
+  $(".playersHand").empty();
+  $(".dealersHand").empty();
+  $("#bet").val(0);
+  $("#betAmount").val(0);
+  $("#amountLeft").val(dplayer.bank);
+  dplayer.currentBet = 0;
+  dplayer.bust = false;
+  ddealer.bust = false;
+  dplayer.hand = [];
+  ddealer.hand = [];
+  gameText("");
+};
+
+const gameText = text => {
+  $(".gameText").text(text);
 };
 
 //Button "click" Event Listeners
@@ -90,44 +183,4 @@ $("#placeBet").on("click", bet);
 $(".hitButton").on("click", hit);
 $(".stayButton").on("click", stay);
 $(".finishButton").on("click", finish);
-
-//If won
-//Add to amount left
-
-//If lost
-//Check if amount left is 0, if so, end game and add an option to try again
-
-//Things to display
-//House's cards
-//Player's cards
-
-// const insertPlayerAmount = e => {
-//   e.preventDefault();
-//   const playerAmount = $("#amountLeft").val();
-//   const dplayer = {};
-//   if (dplayer.bank) {
-//     fetch("/startUp", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       body: JSON.stringify(amountTest)
-//     })
-//       .then(results => results.json())
-//       .then(getPlayer());
-//   }
-// };
-
-// const getPlayer = () => {
-//   fetch("/startUp", {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json"
-//     }
-//   })
-//     .then(response => response.json())
-//     .then(data => {
-//       defaultAmountLeft = data;
-//       console.log(de);
-//     });
-// };
+$(".resetButton").on("click", nextRound);
