c760c6079b84a58de04114c3d5c72026357c258e
cawleygl <grantlcawley@gmail.com>
2021-03-06 02:19:07 -0500

    Style changes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,4 @@
 const Casino = require("../models/casino.js");
-
 // Routes
 module.exports = app => {
   app.get("/api/leaderboard", (req, res) => {
@@ -10,23 +9,18 @@ module.exports = app => {
       res.json(results);
     });
   });
-
   app.post("/api/leaderboard", (req, res) => {
-    console.log(req.body);
     Casino.findAll({
       where: {
         name: req.body.name
       }
     }).then(results => {
-      console.log(results.length);
       if (results.length === 0) {
-        console.log("No name found! - Creating new player");
         Casino.create({
           name: req.body.name,
           balance: req.body.balance
         }).then(results => res.json(results));
       } else {
-        console.log("Player exists - Editing current player");
         Casino.update(
           { balance: req.body.balance },
           {
@@ -36,7 +30,6 @@ module.exports = app => {
           }
         ).then(results => res.json(results));
       }
-      console.log(results);
     });
   });
 };
